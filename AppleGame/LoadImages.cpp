3888a499991c5c26bf253c2a7569b795562c2529
nakamurakouki4 <122337032+nakamurakouki4@users.noreply.github.com>
2023-04-19 09:32:53 +0900

    更新

--- AppleGame/LoadImages.cpp
+++ AppleGame/LoadImages.cpp
@@ -5,9 +5,9 @@
 
 int gAppleImg[4];
 int gStageImg;
+int gTitleImg;
 
 int LoadImages(void) {
 	if (LoadDivGraph("Images/Apple.png", 4, 4, 1, 60, 60, gAppleImg) == -1)return -1;
 	if ((gStageImg = LoadGraph("Images/bg_mori.png")) == -1)return -1;
-	return 0;
 }
