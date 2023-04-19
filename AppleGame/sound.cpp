cc2123fd0508fa458c35606d008d7a7a15e55e86
shimoji-yuu <yuu-shimoji-k22f@stu.kbc.ac.jp>
2023-04-19 10:30:02 +0900

    sounds追加

--- AppleGame/sound.cpp
+++ AppleGame/sound.cpp
@@ -14,19 +14,19 @@ int gSEapple2;				//効果音りんご2(毒りんご)
 int LoadSound(void) 
 {
 	//タイトルBGMの読込
-	if ((gTitleBGM = LoadSoundMem("")) == -1) return -1;
+	if ((gTitleBGM = LoadSoundMem("sounds/hinatabokko.wav")) == -1) return -1;
 	//ステージBGMの読込
-	if ((gStageBGM = LoadSoundMem("")) == -1) return -1;
+	if ((gStageBGM = LoadSoundMem("sounds/MusMus-BGM-087.wav")) == -1) return -1;
 	//ゲームオーバーSEの読込
 	if ((gGameOverSE = LoadSoundMem("")) == -1) return -1;
-	//効果音１の読込
-	if ((gSEmenu1 = LoadSoundMem("")) == -1) return -1;
-	//効果音２の読込
-	if ((gSEmenu2 = LoadSoundMem("")) == -1) return -1;
-	//効果音りんご１の読込
-	if ((gSEapple1 = LoadSoundMem("")) == -1) return -1;
-	//効果音りんご２の読込
-	if ((gSEapple2 = LoadSoundMem("")) == -1) return -1;
+	//効果音１(メニューカーソル移動)の読込
+	if ((gSEmenu1 = LoadSoundMem("sounds/cursor_idou.wav")) == -1) return -1;
+	//効果音２(メニュー決定)の読込
+	if ((gSEmenu2 = LoadSoundMem("sounds/kettei.wav")) == -1) return -1;
+	//効果音りんご１(通常りんご)の読込
+	if ((gSEapple1 = LoadSoundMem("sounds/ringo_get.wav")) == -1) return -1;
+	//効果音りんご２(毒りんご)の読込
+	if ((gSEapple2 = LoadSoundMem("sounds/dokuringo_get2.wav")) == -1) return -1;
 
 	return 0;
 }
