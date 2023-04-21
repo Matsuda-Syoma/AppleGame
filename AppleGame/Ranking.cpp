2c89bf5488aec40d1e2a760bf4fa71e8790747df
shimoji-yuu <yuu-shimoji-k22f@stu.kbc.ac.jp>
2023-04-21 15:04:49 +0900

    ranking追加

--- AppleGame/Ranking.cpp
+++ AppleGame/Ranking.cpp
@@ -1,2 +1,16 @@
 #include "DxLib.h"
 #include "prototype.h"
+#include "variable.h"
+#define RANKING_DATA 5
+
+void DrawRanking(void)
+{
+	// Aボタンでタイトルへ推移
+	
+
+	// ランキング画像表示
+	DrawGraph(0, 0, gTitleImg, FALSE);
+
+	// ランキング一覧を表示
+	
+}
